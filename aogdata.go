dd4d29db7fb0a7cf112dc35760bc45e7bf363795
Jackie Li <li.jackie565@gmail.com>
2024-05-11 23:19:06 -0400

    clean project

--- aogdata.go
+++ aogdata.go
@@ -2,139 +2,90 @@ package aogdata
 
 import (
 	"compress/gzip"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
 
 	"github.com/joho/godotenv"
 )
 
-const RootURL = "https://adventofcode.com"
+const RootUrl = "https://adventofcode.com"
+const (
+	Accept = "Accept"
+	AcceptEncoding = "Accept-Encoding"
+	Cookie = "Cookie"
+) 
 
-func NewAOCData(year int, day int) (string, error) {
-	filterYear, filterDay, err := validateDate(year, day)
-	if err != nil {
-		return "", err
-	}
-
-	token, err := getSessionToken()
-	if err != nil {
-		return "", err
-	}
-
-	url := getURL(filterYear, filterDay)
-
-	data, err := getRequestData(token, url)
-	if err != nil {
-		return "", err
-	}
-
-	return data, nil
+type config struct {
+	year	uint
+	day 	uint
 }
 
-func getSessionToken() (string, error) {
+func NewAdventConfig(year uint, day uint) *config {
+	return &config{
+		year: year,
+		day: day,
+	}
+}
+
+func GetPuzzleInput(config *config) (string) {
 	err := godotenv.Load()
-
 	if err != nil {
-		return "", errors.New("error loading .env file")
+		panic(err)
 	}
 
-	return os.Getenv("AOC_SESSION"), nil
+	sessionToken := os.Getenv("AOC_SESSION")
+
+	return fetchPuzzleInput(sessionToken, buildUrl(config))
 }
 
-func validateDate(year int, day int) (string, string, error) {
-	currentYear := getMostRecentYear(time.Now())
-	isValidYear := year >= 2015 && year <= currentYear
-	isValidDay := day >= 1 && day <= 25
 
-	// checks to see if the newest problem has been released
-	if year == currentYear {
-		currentDay, err := getCurrentESTDay()
-		if err != nil {
-			return "", "", err
-		}
-
-		isValidCurrentDay := day >= 1 && day <= currentDay
-
-		if isValidCurrentDay {
-			isValidDay = isValidCurrentDay
-		} else {
-			return "", "", errors.New("problem hasn't been released yet")
-		}
-	}
-
-	if isValidDay && isValidYear {
-		return strconv.Itoa(year), strconv.Itoa(day), nil
-	} else {
-		return "", "", errors.New("invalid date")
-	}
-}
-
-func getURL(year string, day string) string {	
-	return fmt.Sprintf("%s/%s/day/%s/input", RootURL, year, day)
-}
-
-func getMostRecentYear(currentTime time.Time) int {
-	if currentTime.Month() == time.December && currentTime.Day() >= 1 {
-		return currentTime.Year()
-	} else {
-		return currentTime.Year() - 1
-	}
-}
-
-func getCurrentESTDay() (int, error) {
-	et, err := time.LoadLocation("America/New_York")
+func fetchPuzzleInput(token string, url string) (string) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return 0, err
+		panic(err)
 	}
 
-	return time.Now().In(et).Day(), nil
-}
-
-func getRequestData(token string, url string) (string, error) {
-	req, err := http.NewRequest("GET", url, nil)
-
-	if err != nil {
-		return "", err
-	}
-
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Set("Cookie", fmt.Sprintf("session=%s;", token))
+	req.Header.Set(Accept, "text/plain")
+	req.Header.Set(AcceptEncoding, "gzip")
+	req.Header.Set(Cookie, fmt.Sprintf("session=%s;", token))
 	
 	client := &http.Client{}
 
-	resp, err := client.Do(req)
+	res, err := client.Do(req)
 	if err != nil {
-		return "", errors.New("error sending request")
+		panic(err)
 	}
 
-	defer resp.Body.Close()
+	defer res.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println(resp.StatusCode)
-		if resp.StatusCode == 400 {
-			return "", errors.New("request error, session token invalid")
-		} else {
-			return "", errors.New("request error")
+	if res.StatusCode != http.StatusOK {
+		fmt.Println(res.StatusCode)
+
+		if res.StatusCode == http.StatusNotFound {
+			panic("input problem does not exist")
+		}
+		if res.StatusCode != http.StatusOK {
+			panic(fmt.Sprintf("unhandled status code - %d", res.StatusCode))
 		}
 	}
 
-	reader, err := gzip.NewReader(resp.Body)
+	reader, err := gzip.NewReader(res.Body)
 	if err != nil {
-		return "", errors.New("gzip reading error")
+		panic(err)
 	}
 
 	defer reader.Close()
 
 	body, err := io.ReadAll(reader)
 	if err != nil {
-		return "", errors.New("body reading error")
+		panic(err)
 	}
 
-	return string(body), nil
+	return string(body)
+}
+
+func buildUrl(config *config) string {
+		return fmt.Sprintf("%s/%d/day/%d/input", RootUrl, config.year, config.day)
 }
